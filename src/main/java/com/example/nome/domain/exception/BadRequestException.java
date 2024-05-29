b74bca75b7f67faee7b12d733a111544317ea98b
Rafael-Rech <122633585+Rafael-Rech@users.noreply.github.com>
2024-05-29 11:39:37 -0300

    Delete src directory

--- src/main/java/com/example/nome/domain/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.nome.domain.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String mensagem){
-        super(mensagem);
-    }
-}
--- src/main/java/com/example/nome/domain/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.nome.domain.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String mensagem){
-        super(mensagem);
-    }
-}
